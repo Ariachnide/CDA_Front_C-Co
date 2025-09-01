00b0bebbb1f9d69c1cfcaff144a589ece3973fc8
Ariachnide <rb.guerre@gmail.com>
2025-09-02 01:11:57 +0200

    vite github pages setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import githubPages from "vite-plugin-github-pages";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), githubPages()],
+  base: "/CDA_Front_C-Co/"
+});
